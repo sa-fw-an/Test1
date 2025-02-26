d49e9ac80b0760b8605d64935b70a6d95613e6b6
sa-fw-an <isafwansayeed@gmail.com>
2025-02-26 17:43:50 +0530

    Test

--- src/constants/Donation.ts
+++ src/constants/Donation.ts
@@ -12,5 +12,5 @@ export const donationData: donationData = {
     "Support Sugar Labs and make a difference in children's education. Sugar Labs has brought educational software and authentic problem-solving to millions in the US and across the globe.",
   subHeading: 'Donate to Sugar Labs to make a positive impact',
   url: 'https://www.paypal.com/donate?campaign_id=NEAV3YL4H6B5S',
-  buttonText: 'Donate now',
+  buttonText: 'Donate now ',
 };
