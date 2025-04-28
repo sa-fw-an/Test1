a4328c6552f72aac8665d33ed23a7d6bed460450
Safwan Sayeed <isafwansayeed@gmail.com>
2025-04-28 21:45:30 +0530

    Update

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,9 @@
+// main.tsx (or index.tsx)
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
+import 'core-js/proposals/global-this';
 import 'whatwg-fetch';
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from '@/App';
